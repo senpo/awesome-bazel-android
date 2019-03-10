f08e36cc1fcda9d418e5df8510202dd02e884997
Senpo Hu <senpo.hu@gmail.com>
2019-03-10 12:42:27 -0700

    Add a workspace for testing bazelbuild-gmaven_rules.

---
+++ bazelbuild-gmaven_rules/java/com/awesome/android/helloworld/MainActivity.java
@@ -0,0 +1,15 @@
+package com.awesome.android.helloworld;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends Activity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    Log.v("MainActivity", "Hello, Android");
+  }
+}
