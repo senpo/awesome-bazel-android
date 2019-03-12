981025ec5f41d7ae1842af72af00a30d17842787
Senpo Hu <senpo.hu@gmail.com>
2019-03-12 11:03:15 -0700

    Enable AutoValue in the workspace bazelbuild-rules_jvm_external.

--- bazelbuild-rules_jvm_external/java/com/awesome/android/helloworld/MainActivity.java
+++ bazelbuild-rules_jvm_external/java/com/awesome/android/helloworld/MainActivity.java
@@ -6,10 +6,15 @@ import android.util.Log;
 
 public class MainActivity extends Activity {
 
+  private static final String TAG = "helloworld";
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-    Log.v("MainActivity", "Hello, Android");
+    Log.v(TAG, "Hello, Android");
+
+    Animal animal = Animal.create("Lion", 4);
+    Log.i(TAG, "animal: " + animal);
   }
 }
